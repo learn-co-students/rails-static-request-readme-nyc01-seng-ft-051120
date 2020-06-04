3124f18578be79e8916dc79a5a16e692dacdd587
JulianCedric <julian.pormentilla@gmail.com>
2020-06-04 01:19:56 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "about", to: "static#about"
+
+  # get               =>      the HTTP verb 
+  # 'about'           =>      the path in the URL bar that the route will be mapped to
+  # 'static#about'    =>      tells the Rails routing system that this route should be 
+    #                         passed through the 'static' controller's 'about' action.
+
 end
